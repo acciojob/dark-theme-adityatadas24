1c6abb4deae09c057d4278e60bf28da58a0d0e79
aditya tadas <141910946+adityatadas24@users.noreply.github.com>
2023-09-05 18:39:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 //your code here
+// Get references to the elements
+const appDiv = document.getElementById("app");
+const swapButton = document.getElementById("swap");
+
+// Function to toggle the theme
+function swapTheme() {
+    // Toggle the 'day' and 'night' classes on the appDiv
+    appDiv.classList.toggle("day");
+    appDiv.classList.toggle("night");
+
+    // Toggle the text and class on the swapButton
+    if (appDiv.classList.contains("night")) {
+        swapButton.textContent = "Switch to Light Theme";
+        swapButton.classList.remove("button_day");
+        swapButton.classList.add("button_night");
+    } else {
+        swapButton.textContent = "Switch to Dark Theme";
+        swapButton.classList.remove("button_night");
+        swapButton.classList.add("button_day");
+    }
+}
+
+// Add a click event listener to the button
+swapButton.addEventListener("click", swapTheme);
+
 
